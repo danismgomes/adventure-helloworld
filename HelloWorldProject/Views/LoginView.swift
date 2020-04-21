49de1947f8c3d5ac6c361e513eafc5274e4e0c74
Danielle <Gomes>
2020-04-21 15:46:00 -0700

    Add firebase and realm pods

---
+++ HelloWorldProject/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  HelloWorldProject
+//
+//  Created by Danielle Gomes on 2020-04-21.
+//  Copyright © 2020 Danielle Gomes. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
