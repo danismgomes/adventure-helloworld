249f36a8689ee9b1307e529e93d95b75223a33e1
Danielle <Gomes>
2020-04-21 21:21:37 -0700

    Add logout

--- HelloWorldProject/Views/LoginView/SignInView.swift
+++ HelloWorldProject/Views/LoginView/SignInView.swift
@@ -9,13 +9,51 @@
 import SwiftUI
 
 struct SignInView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    private let AppLogoImageName = "PocketLogo"
+    
+    @EnvironmentObject var session: FirebaseSession
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 30){
+                Image(AppLogoImageName)
+                    .frame(width: 320.0, height: 320.0)
+                    .aspectRatio(contentMode: .fit)
+                VStack(spacing: 20){
+                    TextField("E-mail" , text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal, 45)
+                        .font(Font.custom("Helvetica", size: 12))
+                    TextField("Password" , text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal, 45)
+                        .font(Font.custom("Helvetica", size: 12))
+                }
+                Button(action: logIn){
+                    Text("SIGN IN")
+                        .foregroundColor(Color.white)
+                        .font(Font.custom("HelveticaNeue-Medium", size: 16))
+                }
+                .frame(width: 105.0, height: 45.0)
+                .background(Color.init(UIColor.brown))
+                .cornerRadius(20)
+            }
+        }
     }
-}
-
-struct SignInView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignInView()
+    
+    //MARK: Functions
+    func logIn() {
+        session.logIn(email: email, password: password) { (result, error) in
+            if error != nil {
+                print("Error")
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
     }
 }
--- HelloWorldProject/Views/MainView.swift
+++ HelloWorldProject/Views/MainView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @EnvironmentObject var session: FirebaseSession
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Text("Hello World!")
+                .tabItem {Text("HW")}
+            
+            Button(action: session.logOut)  {
+            Text("Logout")
+                .foregroundColor(Color.white)
+                .font(Font.custom("HelveticaNeue-Medium", size: 20))
+            }
+            .frame(width: 105.0, height: 45.0)
+            .background(Color.init(UIColor.brown))
+            .cornerRadius(20)
+            .tabItem {Text("LogOut")}
+        }
     }
 }
 
